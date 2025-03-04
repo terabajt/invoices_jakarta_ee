31e021983997ffb02e08888136332bb3da9357ff
Michał Pasynek <terabajt@terabajtlw.pl>
2025-03-04 10:59:17 +0100

    [feat] added PrimeFaces dependency and configuration

---
+++ src/main/java/pl/javastart/invoices/HelloBean.java
@@ -0,0 +1,14 @@
+package pl.javastart.invoices;
+
+
+public class HelloBean {
+    private String message = "Witaj w PrimeFaces!";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
